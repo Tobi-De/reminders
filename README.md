38a2fb5a2a6710e1a307d727277073d4fc823b25
Tobi DEGNON <tobidegnon@proton.me>
2024-02-10 20:04:36 +0100

    fix run commands

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 docker build -t reminders . -f deploy/Dockerfile 
 docker run --name reminders -p 8005:8000 reminders
 ```
+
+s6-rc: fatal: timed out
+s6-sudoc: fatal: unable to get exit status from server: Operation timed out
--- deploy/etc/s6-overlay/scripts/db-prepare.sh
+++ deploy/etc/s6-overlay/scripts/db-prepare.sh
@@ -1,9 +1,4 @@
 #!/command/with-contenv sh
 
-alias python="/opt/pysetup/.venv/bin/python"
-
 cd /app
-
-python manage.py migrate
-python manage.py shell < create_user.py
-python manage.py setup_periodic_tasks
+/opt/pysetup/.venv/bin/python manage.py migrate
