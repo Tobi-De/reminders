1a40c7ee38041d8af9299e7cb2a104b05cc40a75
Tobi DEGNON <tobidegnon@proton.me>
2024-02-11 17:29:40 +0100

    remove verbose

--- deploy/etc/s6-overlay/scripts/db-prepare.sh
+++ deploy/etc/s6-overlay/scripts/db-prepare.sh
@@ -5,8 +5,9 @@ if [ -f /data/db ]; then
 	echo "Database already exists, skipping restore"
 else
 	echo "No database found, restoring from replica if exists"
-	exec litestream restore -v -if-replica-exists -o /data/db "${REPLICA_URL}"
+	exec litestream restore -if-replica-exists -o /data/db "${REPLICA_URL}"
 fi
 
+# Migrate the database
 cd /app
 python manage.py migrate
